7367d6ce73f3a5db4b846faeba979b2c42821758
vishkharat <154818939+vishkharat@users.noreply.github.com>
2024-04-23 11:34:08 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+// Connection to mongodb.
+mongoose.connect("mongodb+srv://vishdcosta:D4cUhjAHxrAOnpNy@cluster0.7tutnwy.mongodb.net/employeereviewsystem"); 
+
+const db = mongoose.connection; // db store the connection
+
+// cheacking the connection
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("DATABASE connection is Established");
+});
+
+// exporting the connection.
+module.exports = db;
